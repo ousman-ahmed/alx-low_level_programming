dde970af56bb098cb4e932ba87f976c9b142df21
Ousman Ahmed <ousrab2@gmail.com>
2022-09-14 04:12:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
+* main - the main function
 * Return: 0 for success
 */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
