1a0d2ddbe42b1667a689dfaa366dc0499bb56bfb
Ousman Ahmed <ousrab2@gmail.com>
2022-09-14 20:32:39 -0700

    putchat

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
-#inculde "main.h"
+#include "main.h"
 
 /**
-* main - the entry function
-* Return: 0 for success
+ * main - print putchar
+ * Return: always 0
 */
+
 int main(void)
 {
-	char *s;
-	int i;
-
-	s = "_putchar\n";
-	i = 0;
-	while (s[i] != '\n')
-		_putchar(s[i++]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
