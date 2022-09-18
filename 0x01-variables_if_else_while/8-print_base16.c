59ba8c5066e2c54d33bc12a2ded768b04ff35c45
Ousman Ahmed <ousrab2@gmail.com>
2022-09-18 02:20:15 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#Include <stdio.h>
+
+/**
+ * main - The entry
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+	char nl;
+
+	i = 0x64;
+	nl = '\n';
+	while (i <= 0x6)
+		putchar(i++);
+	putchar(nl);
+	return (0);
+}
