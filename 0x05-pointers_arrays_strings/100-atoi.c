c17ee3c0c44e85e8862d4a485269a19859e985f5
Ousman Ahmed <ousrab2@gmail.com>
2022-09-19 05:22:45 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,14 +16,14 @@ int _atoi(char *s)
 
 	while (s[c])
 	{
-		if (s[c]) ==45)
+		if (s[c]) == 45)
 		{
 			min *= -1;
 		}
 		while (s[c] >= 48 && s[c] <= 57)
 		{
 			isi = 1;
-			ni = (ni * 10) + (s[c] -'0')
+			ni = (ni * 10) + (s[c] -'0');
 			c++;
 		}
 		if (isi == 1)
