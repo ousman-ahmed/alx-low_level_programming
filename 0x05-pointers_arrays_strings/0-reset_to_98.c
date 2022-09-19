b8b7354b0a69116de187c53df922643ea74a8005
Ousman Ahmed <ousrab2@gmail.com>
2022-09-19 02:08:31 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset the arguments value of the pointer to 98
+ * @n: pointer to integer
+ * Return:void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+allah is great
+
