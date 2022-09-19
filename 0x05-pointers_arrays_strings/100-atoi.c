2bb1a717768fe90d6790f05293db87dafb3a09ef
Ousman Ahmed <ousrab2@gmail.com>
2022-09-19 05:15:57 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,12 +3,36 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: pointer for the string argument
+ * @s: The pointer convert
+ *
  * Return: the converted integer
  */
 int _atoi(char *s)
 {
-	int n;
-	n = atoi(s);
-	return (n);
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
+
+	while (s[c])
+	{
+		if (s[c]) ==45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] -'0')
+			c++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
+	}
+	ni *= min;
+	return (ni);
 }
+
