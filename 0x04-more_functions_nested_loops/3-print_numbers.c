a99c600ed0a178ff9560ff74342c3704b1ace1da
Ousman Ahmed <ousrab2@gmail.com>
2022-09-21 06:43:13 -0700

    pnumber

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0 to 9
- * Return: void
+ * print_numbers - Prints the numbers from 0-9.
  */
 void print_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 48; i < 58; i++)
-		_putchar(i);
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
 	_putchar('\n');
 }
