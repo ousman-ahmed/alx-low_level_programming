036e75d8681f1056c3bdccf3d42e813312ba7eff
Ousman Ahmed <ousrab2@gmail.com>
2022-09-22 01:35:37 -0700

    update cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,43 @@
-#include "main.h"
-#include <string.h>
-#include <stdio.h>
+/*
+ * File: 6-cap_string.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 
 /**
- * cap_string - capitalizes all words of a string.
- * @str: pointer for the string
- * Return: pointer for the capitalized string.
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
 {
-	int i;
-	int j;
-	char sep[] = " \t\n,;.!?\"(){}";
-	
-	printf("%lu\n", sizeof(sep));
-	for (i = 0; *(str + i) != '\0'; i++)
-		for (j = 0; sep[j] != '\0'; j++)
-			;
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
+	}
+
 	return (str);
 }
-
