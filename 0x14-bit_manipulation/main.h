6d94ae043c1d46a55afbd76737037c56764889b2
Ousman Ahmed <ousman-ahmed.com>
2022-10-28 10:52:48 +0300

    task 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: main.h
- * AUth: ouus
+ * AUth: ous
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-bit_manipulation directory.
  */
@@ -18,3 +18,4 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 
 #endif /* MAIN_H */
+
